73bfb757f172b8307d0982758dc7bae9f476a02d
SinghalSahab <prakharsinghal10a@gmail.com>
2024-12-29 03:05:34 +0530

    solve the errors in data providers

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Route, Routes } from "react-router";
-
-
+ 
+import {  dataProvider, liveProvider } from "./provider/data";
 
 function App() {
   return (
@@ -26,8 +22,8 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                //dataProvider={dataProvider(gqlClient)}
-                //liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 //authProvider={authProvider}
