aad1907d1541a7326d3506f72929ea0fbe6835c7
SinghalSahab <prakharsinghal10a@gmail.com>
2025-01-01 06:58:21 +0530

    dashboard start

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router";
  
 import {  dataProvider, liveProvider , authProvider } from "./provider/Index";
 import Layout from "./components/layout";
+import { resources } from "./config/resources";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
